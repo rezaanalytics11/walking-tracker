7ee12ca03ed518ed363f178d8854558e210353e7
Reza <105513524+rezaanalytics11@users.noreply.github.com>
2026-06-22 15:56:46 +0330

    Add files via upload

---
+++ app.js
@@ -0,0 +1,78 @@
+const supabaseUrl =
+"https://YOURPROJECT.supabase.co"
+
+const supabaseKey =
+"YOUR_ANON_KEY"
+
+const client =
+supabase.createClient(
+supabaseUrl,
+supabaseKey
+)
+
+async function saveWalk(){
+
+const date =
+document.getElementById("date").value
+
+const hours =
+parseFloat(document.getElementById("hours").value)
+
+await client
+.from("walking")
+.insert({
+
+date:date,
+
+hours:hours
+
+})
+
+loadChart()
+
+}
+
+async function loadChart(){
+
+const {data,error} =
+await client
+
+.from("walking")
+
+.select("*")
+
+.order("date")
+
+const labels =
+data.map(x=>x.date)
+
+const values =
+data.map(x=>x.hours)
+
+new Chart(
+
+document.getElementById("myChart"),
+
+{
+
+type:"line",
+
+data:{
+
+labels:labels,
+
+datasets:[{
+
+label:"ساعت پیاده روی",
+
+data:values
+
+}]
+
+}
+
+})
+
+}
+
+loadChart()
